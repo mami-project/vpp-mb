5adedddba1690bd88089630ebf6b3a2ea0728008
IurmanJ <justin.iurman@ulg.ac.be>
2017-10-15 19:36:36 +0200

    mmb plugin parser in progress (matching part)

--- mmb-plugin/mmb/mmb.c
+++ mmb-plugin/mmb/mmb.c
@@ -54,9 +54,7 @@
 
 /* List of message types that this plugin understands */
 
-#define foreach_mmb_plugin_api_msg                                   \
-_(MMB_DUMBREWRITE_ENABLE_DISABLE, mmb_dumbrewrite_enable_disable)    \
-_(MMB_VALUEBASED_ENABLE_DISABLE, mmb_valuebased_enable_disable)
+#define foreach_mmb_plugin_api_msg
 
 /* *INDENT-OFF* */
 VLIB_PLUGIN_REGISTER () = {
@@ -65,204 +63,388 @@ VLIB_PLUGIN_REGISTER () = {
 };
 /* *INDENT-ON* */
 
-/**
- * @brief Enable/disable the dumb-rewrite plugin. 
- *
- * Action function shared between message handler and debug CLI.
- */
 
-int mmb_dumbrewrite_enable_disable (mmb_main_t * sm, u32 sw_if_index,
-                                   int enable_disable)
-{
-  vnet_sw_interface_t * sw;
-  int rv = 0;
 
-  /* Utterly wrong? */
-  if (pool_is_free_index (sm->vnet_main->interface_main.sw_interfaces, 
-                          sw_if_index))
-    return VNET_API_ERROR_INVALID_SW_IF_INDEX;
+#define MMB_UNKNOWN		0
+//
+#define MMB_TYPE_UNKNOWN	MMB_UNKNOWN
+#define MMB_TYPE_FIELD		1
+#define MMB_TYPE_CONDITION	2
+#define MMB_TYPE_TARGET		3
+//
+#define BEGIN_MMB_CONDITIONS	4
+#define MMB_COND_EQ		5
+#define MMB_COND_NEQ		6
+#define MMB_COND_LEQ		7
+#define MMB_COND_GEQ		8
+#define MMB_COND_L		9
+#define MMB_COND_G		10
+#define END_MMB_CONDITIONS	11
+//
+#define BEGIN_MMB_TARGETS	12
+#define MMB_TARGET_DROP		13
+#define MMB_TARGET_STRIP	14
+#define MMB_TARGET_MODIFY	15
+#define END_MMB_TARGETS		16
+//
+#define BEGIN_MMB_FIELDS	17
+#define MMB_FIELD_IP_PROTO	18
+#define END_MMB_FIELDS		19
 
-  /* Not a physical port? */
-  sw = vnet_get_sw_interface (sm->vnet_main, sw_if_index);
-  if (sw->type != VNET_SW_INTERFACE_TYPE_HARDWARE)
-    return VNET_API_ERROR_INVALID_SW_IF_INDEX;
-  
-  //TODO: change value "device-input" (wrong arc-name for what we want)
-  vnet_feature_enable_disable ("device-input", "mmb",
-                               sw_if_index, enable_disable, 0, 0);
 
-  return rv;
-}
 
 static clib_error_t *
-dumbrewrite_enable_disable_command_fn (vlib_main_t * vm,
-                                   unformat_input_t * input,
-                                   vlib_cli_command_t * cmd)
+enable_command_fn (vlib_main_t * vm,
+                   unformat_input_t * input,
+                   vlib_cli_command_t * cmd)
 {
-  mmb_main_t * sm = &mmb_main;
-  u32 sw_if_index = ~0;
-  int enable_disable = 1;
-    
-  int rv;
+  if (!unformat_is_eof(input))
+    return clib_error_return(0, "Syntax error: unexpected additional element");
 
-  while (unformat_check_input (input) != UNFORMAT_END_OF_INPUT) {
-    if (unformat (input, "disable"))
-      enable_disable = 0;
-    else if (unformat (input, "%U", unformat_vnet_sw_interface,
-                       sm->vnet_main, &sw_if_index))
-      ;
-    else
-      break;
-  }
-
-  if (sw_if_index == ~0)
-    return clib_error_return (0, "Please specify an interface...");
-    
-  rv = mmb_dumbrewrite_enable_disable (sm, sw_if_index, enable_disable);
-
-  switch(rv) {
-  case 0:
-    break;
-
-  case VNET_API_ERROR_INVALID_SW_IF_INDEX:
-    return clib_error_return 
-      (0, "Invalid interface, only works on physical ports");
-    break;
-
-  case VNET_API_ERROR_UNIMPLEMENTED:
-    return clib_error_return (0, "Device driver doesn't support redirection");
-    break;
-
-  default:
-    return clib_error_return (0, "mmb_dumbrewrite_enable_disable returned %d",
-                              rv);
-  }
+  //TODO: enable
   return 0;
 }
 
-/**
- * @brief Enable/disable the value-based plugin. 
- *
- * Action function shared between message handler and debug CLI.
- */
-
-int mmb_valuebased_enable_disable (mmb_main_t * sm, u32 sw_if_index,
-                                   int enable_disable)
-{
-  vnet_sw_interface_t * sw;
-  int rv = 0;
-
-  /* Utterly wrong? */
-  if (pool_is_free_index (sm->vnet_main->interface_main.sw_interfaces, 
-                          sw_if_index))
-    return VNET_API_ERROR_INVALID_SW_IF_INDEX;
-
-  /* Not a physical port? */
-  sw = vnet_get_sw_interface (sm->vnet_main, sw_if_index);
-  if (sw->type != VNET_SW_INTERFACE_TYPE_HARDWARE)
-    return VNET_API_ERROR_INVALID_SW_IF_INDEX;
-  
-  //TODO: change value "device-input" (wrong arc-name for what we want)
-  //TODO: maybe not the right way: how to distinguish this with dumb-rewrite ???
-  vnet_feature_enable_disable ("device-input", "mmb",
-                               sw_if_index, enable_disable, 0, 0);
-
-  return rv;
-}
-
 static clib_error_t *
-valuebased_enable_disable_command_fn (vlib_main_t * vm,
-                                   unformat_input_t * input,
-                                   vlib_cli_command_t * cmd)
+disable_command_fn (vlib_main_t * vm,
+                   unformat_input_t * input,
+                   vlib_cli_command_t * cmd)
 {
-  mmb_main_t * sm = &mmb_main;
-  u32 sw_if_index = ~0;
-  int enable_disable = 1;
-    
-  int rv;
+  if (!unformat_is_eof(input))
+    return clib_error_return(0, "Syntax error: unexpected additional element");
 
-  while (unformat_check_input (input) != UNFORMAT_END_OF_INPUT) {
-    if (unformat (input, "disable"))
-      enable_disable = 0;
-    else if (unformat (input, "%U", unformat_vnet_sw_interface,
-                       sm->vnet_main, &sw_if_index))
-      ;
-    else
-      break;
-  }
-
-  if (sw_if_index == ~0)
-    return clib_error_return (0, "Please specify an interface...");
-    
-  rv = mmb_valuebased_enable_disable (sm, sw_if_index, enable_disable);
-
-  switch(rv) {
-  case 0:
-    break;
-
-  case VNET_API_ERROR_INVALID_SW_IF_INDEX:
-    return clib_error_return 
-      (0, "Invalid interface, only works on physical ports");
-    break;
-
-  case VNET_API_ERROR_UNIMPLEMENTED:
-    return clib_error_return (0, "Device driver doesn't support redirection");
-    break;
-
-  default:
-    return clib_error_return (0, "mmb_dumbrewrite_enable_disable returned %d",
-                              rv);
-  }
+  //TODO: disable
   return 0;
 }
 
+static clib_error_t *
+display_rules_command_fn (vlib_main_t * vm,
+                          unformat_input_t * input,
+                          vlib_cli_command_t * cmd)
+{
+  if (!unformat_is_eof(input))
+    return clib_error_return(0, "Syntax error: unexpected additional element");
+
+  //TODO: display all rules
+  return 0;
+}
+
+u8 get_unique_id(char* element)
+{
+  /* Conditions */
+  if (!strcmp(element, "=="))
+    return MMB_COND_EQ;
+  if (!strcmp(element, "!="))
+    return MMB_COND_NEQ;
+  if (!strcmp(element, "<="))
+    return MMB_COND_LEQ;
+  if (!strcmp(element, ">="))
+    return MMB_COND_GEQ;
+  if (!strcmp(element, "<"))
+    return MMB_COND_L;
+  if (!strcmp(element, ">"))
+    return MMB_COND_G;
+
+  /* Targets */
+  if (!strcmp(element, "drop"))
+    return MMB_TARGET_DROP;
+  if (!strcmp(element, "strip"))
+    return MMB_TARGET_STRIP;
+  if (!strcmp(element, "mod"))
+    return MMB_TARGET_MODIFY;
+
+  /* Fields */
+  if (!strcmp(element, "ip-proto"))
+    return MMB_FIELD_IP_PROTO;
+
+  /* Unknown... */
+  return MMB_UNKNOWN;
+}
+
+u8 get_type(u8 id)
+{
+  /* Element belongs to the conditions group */
+  if (id > BEGIN_MMB_CONDITIONS && id < END_MMB_CONDITIONS)
+    return MMB_TYPE_CONDITION;
+
+  /* Element belongs to the targets group */
+  if (id > BEGIN_MMB_TARGETS && id < END_MMB_TARGETS)
+    return MMB_TYPE_TARGET;
+
+  /* Element belongs to the fields group */
+  if (id > BEGIN_MMB_FIELDS && id < END_MMB_FIELDS)
+    return MMB_TYPE_FIELD;
+
+  /* Element does not belong to any known group */
+  return MMB_TYPE_UNKNOWN;
+}
+
+u8 is_value(char * str)
+{
+  char* tmp = str;
+
+  //TODO: include "-" (minus) for negatives, "." (dot) for floating numbers, "0x" for hex values
+  while(*tmp != '\0')
+  {
+    if (*tmp < '0' || *tmp > '9')
+      return 0;
+
+    tmp++;
+  }
+
+  return 1;
+}
+
+static clib_error_t *
+add_rule_command_fn (vlib_main_t * vm, unformat_input_t * input, vlib_cli_command_t * cmd)
+{
+  u8 next_id, next_type;
+
+  /* Read first element - must be an existing field */
+
+  char* field;
+  if (!unformat(input, "%s", &field))
+    return clib_error_return(0, "Missing field: use \"list fields\" command for help");
+
+  u8 id = get_unique_id(field);
+  u8 type = get_type(id);
+
+  if (id == MMB_UNKNOWN || type != MMB_TYPE_FIELD)
+    return clib_error_return(0, "Unknown field \"%s\": use \"list fields\" command for help", field);
+
+  vlib_cli_output(vm, "Field = %s\n", field);
+
+mmb_match_field:
+  /*
+   * Current element is a field (matching part)
+   *
+   * Next element can either be:
+   *   - another field
+   *   - a matching condition
+   *   - a value (implicit "==" condition)
+   *   - a target action (when fields matching ends)
+  */;
+
+  char* field_next;
+  if (!unformat(input, "%s", &field_next))
+    return clib_error_return(0, "Unexpected end of command line: a field must be followed by either another field, a condition, a value or a target action");
+
+  next_id = get_unique_id(field_next);
+  next_type = get_type(next_id);
+
+  switch(next_type)
+  {
+    /* next element is a field */
+    case MMB_TYPE_FIELD:
+      vlib_cli_output(vm, "Next is a field = %s\n", field_next);
+      goto mmb_match_field;
+
+    /* next element is a condition */
+    case MMB_TYPE_CONDITION:
+      vlib_cli_output(vm, "Next is a condition = %s\n", field_next);
+      goto mmb_match_condition;
+
+    /* next element is a target */
+    case MMB_TYPE_TARGET:
+      switch(next_id)
+      {
+        case MMB_TARGET_DROP:
+          vlib_cli_output(vm, "Next is a DROP target (%s)\n", field_next);
+          goto mmb_target_drop;
+
+        case MMB_TARGET_STRIP:
+          vlib_cli_output(vm, "Next is a STRIP target (%s)\n", field_next);
+          goto mmb_target_strip;
+
+        case MMB_TARGET_MODIFY:
+          vlib_cli_output(vm, "Next is a MOD target (%s)\n", field_next);
+          goto mmb_target_modify;
+
+        default:
+          return clib_error_return(0, "Unexpected target element \"%s\"", field_next);
+      }
+
+    default:
+      /* Maybe it is a value so check it */
+      if (is_value(field_next))
+      {
+        vlib_cli_output(vm, "Next is a value = %s\n", field_next);
+        goto mmb_match_value;
+      }
+
+      /* Something unknown/unexpected */
+      return clib_error_return(0, "Unexpected element \"%s\": a field must be followed by either another field, a condition, a value or a target action", field_next);
+  }
+
+mmb_match_condition:
+  /*
+   * Current element is a condition (matching part)
+   *
+   * Next element must be a value
+  */;
+
+  char* cond_next;
+  if (!unformat(input, "%s", &cond_next))
+    return clib_error_return(0, "Unexpected end of command line: a condition must be followed by a value");
+
+  next_id = get_unique_id(cond_next);
+  next_type = get_type(next_id);
+
+  /* MUST be a value next */
+  if (next_type != MMB_TYPE_UNKNOWN || !is_value(cond_next))
+    return clib_error_return(0, "Unexpected element \"%s\": a condition must be followed by a value", cond_next);
+
+  vlib_cli_output(vm, "Next is a value = %s\n", cond_next);
+
+mmb_match_value:
+  /*
+   * Current element is a value (matching part)
+   *
+   * Next element can either be:
+   *   - a field
+   *   - a target action (when fields matching ends)
+  */;
+
+  char* value_next;
+  if (!unformat(input, "%s", &value_next))
+    return clib_error_return(0, "Unexpected end of command line: a value must be followed by either a field or a target action");
+
+  next_id = get_unique_id(value_next);
+  next_type = get_type(next_id);
+
+  switch(next_type)
+  {
+    /* next element is a field */
+    case MMB_TYPE_FIELD:
+      vlib_cli_output(vm, "Next is a field = %s\n", value_next);
+      goto mmb_match_field;
+
+    /* next element is a target */
+    case MMB_TYPE_TARGET:
+      switch(next_id)
+      {
+        case MMB_TARGET_DROP:
+          vlib_cli_output(vm, "Next is a DROP target (%s)\n", value_next);
+          goto mmb_target_drop;
+
+        case MMB_TARGET_STRIP:
+          vlib_cli_output(vm, "Next is a STRIP target (%s)\n", value_next);
+          goto mmb_target_strip;
+
+        case MMB_TARGET_MODIFY:
+          vlib_cli_output(vm, "Next is a MOD target (%s)\n", value_next);
+          goto mmb_target_modify;
+
+        default:
+          return clib_error_return(0, "Unexpected target element \"%s\"", value_next);
+      }
+
+    /* Anything else unexpected... */
+    default:
+      return clib_error_return(0, "Unexpected element \"%s\": a value must be followed by either a field or a target action", value_next);
+  }
+
+mmb_target_drop:
+
+  //TODO
+
+mmb_target_strip:
+
+  //TODO
+
+mmb_target_modify:
+
+  //TODO
+
+/*
+mmb_target__after_strip_field:
+
+  //TODO
+
+mmb_target__after_modify_field:
+
+  //TODO
+
+mmb_target__after_modify_value:
+
+  //TODO
+
+mmb_target_eof:
+*/
+
+  //TODO
+  return 0;
+}
+
+static clib_error_t *
+del_rule_command_fn (vlib_main_t * vm,
+                   unformat_input_t * input,
+                   vlib_cli_command_t * cmd)
+{
+  u32 ruleId;
+
+  if (unformat(input, "%u", &ruleId) && ruleId > 0)
+  {
+    if (unformat_is_eof(input))
+    {
+      //TODO: remove rule ruleId
+      return 0;
+    }
+
+    return clib_error_return(0, "Syntax error: unexpected additional element");
+  }
+
+  return clib_error_return(0, "Syntax error: rule number must be an integer greater than 0");
+}
+
+
+
 /**
- * @brief CLI command to enable/disable the mmb dumb-rewrite plugin.
+ * @brief CLI command to enable the mmb plugin.
  */
-VLIB_CLI_COMMAND (sr_content_command1, static) = {
-    .path = "mmb dumb-rewrite",
-    .short_help = "mmb dumb-rewrite <interface-name> [disable]",
-    .function = dumbrewrite_enable_disable_command_fn,
+VLIB_CLI_COMMAND (sr_content_command_enable, static) = {
+    .path = "mmb enable",
+    .short_help = "Enable the MMB plugin",
+    .function = enable_command_fn,
+};
+
+/**
+ * @brief CLI command to disable the mmb plugin.
+ */
+VLIB_CLI_COMMAND (sr_content_command_disable, static) = {
+    .path = "mmb disable",
+    .short_help = "Disable the MMB plugin",
+    .function = disable_command_fn,
+};
+
+/**
+ * @brief CLI command to list all rules.
+ */
+VLIB_CLI_COMMAND (sr_content_command_display_rules, static) = {
+    .path = "mmb list",
+    .short_help = "Display all rules",
+    .function = display_rules_command_fn,
+};
+
+/**
+ * @brief CLI command to add a new rule.
+ */
+VLIB_CLI_COMMAND (sr_content_command_add_rule, static) = {
+    .path = "mmb add",
+    .short_help = "Add a rule: mmb add <field> [[<cond>] <value>] [<field> [[<cond>] <value>] ...] <strip <option-field> [strip|mod ...]|mod [<field>] <value> [strip|mod ...]|drop>",
+    .function = add_rule_command_fn,
+};
+
+/**
+ * @brief CLI command to remove a rule.
+ */
+VLIB_CLI_COMMAND (sr_content_command_del_rule, static) = {
+    .path = "mmb del",
+    .short_help = "Remove a rule: mmb del <rule-number>",
+    .function = del_rule_command_fn,
 };
 
 
-/**
- * @brief CLI command to enable/disable the mmb value-based plugin.
- */
-VLIB_CLI_COMMAND (sr_content_command2, static) = {
-    .path = "mmb value-based",
-    .short_help = "mmb value-based <interface-name> [disable]", //can be "REWRITE" or "DROP" for each (group of) rule(s)
-    .function = valuebased_enable_disable_command_fn,
-};
-
-/**
- * @brief Plugin API message handler.
- */
-static void vl_api_mmb_dumbrewrite_enable_disable_t_handler
-(vl_api_mmb_dumbrewrite_enable_disable_t * mp)
-{
-  vl_api_mmb_dumbrewrite_enable_disable_reply_t * rmp;
-  mmb_main_t * sm = &mmb_main;
-  int rv;
-
-  rv = mmb_dumbrewrite_enable_disable (sm, ntohl(mp->sw_if_index), 
-                                      (int) (mp->enable_disable));
-  
-  REPLY_MACRO(VL_API_MMB_DUMBREWRITE_ENABLE_DISABLE_REPLY);
-}
-
-static void vl_api_mmb_valuebased_enable_disable_t_handler
-(vl_api_mmb_valuebased_enable_disable_t * mp)
-{
-  vl_api_mmb_valuebased_enable_disable_reply_t * rmp;
-  mmb_main_t * sm = &mmb_main;
-  int rv;
-
-  rv = mmb_valuebased_enable_disable (sm, ntohl(mp->sw_if_index), 
-                                      (int) (mp->enable_disable));
-  
-  REPLY_MACRO(VL_API_MMB_VALUEBASED_ENABLE_DISABLE_REPLY);
-}
 
 /**
  * @brief Set up the API message handling tables.
