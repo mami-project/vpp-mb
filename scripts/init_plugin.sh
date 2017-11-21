97132ad9b4eac1218f6005e9ad1af331a3e96d44
korian <korian.edeline@ulg.ac.Be>
2017-11-21 12:41:10 +0100

    test scripts

---
+++ scripts/init_plugin.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo vppctl -s /run/vpp/cli-vpp1.sock mmb enable host-vpp1out 
+sudo vppctl -s /run/vpp/cli-vpp1.sock mmb enable host-vpp1vpp2
+
+sudo vppctl -s /run/vpp/cli-vpp1.sock mmb add ip-proto tcp mod ip-dscp 1234
+#sudo vppctl -s /run/vpp/cli-vpp1.sock mmb add ip-proto tcp mod ip-dscp 1234
+#sudo vppctl -s /run/vpp/cli-vpp1.sock mmb add ip-proto tcp mod ip-dscp 1234
+
+
