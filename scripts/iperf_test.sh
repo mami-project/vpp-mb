73190d8ad9ab3cf20bd9c6dedb517891a75df8f5
IurmanJ <justin.iurman@ulg.ac.be>
2017-12-12 19:04:37 +0100

    iperf test: use network namespaces

--- scripts/iperf_test.sh
+++ scripts/iperf_test.sh
@@ -2,52 +2,75 @@
 
 # TOPOLOGY:
 # =========
-#    _________                                                      __________
-#   |         |    host-vppiperf                     iperfvpp      |          |
-#   |   VPP   |--- 10.10.2.1/24 ------------------ 10.10.2.2/24 ---|  SERVER  |
-#   |_________|                                                    |__________|
-#        |
-#    host-vppout
-#   10.10.1.2/24
-#        |
-#        |
+#    _________                                              _____________
+#   |         |     host-vpp2                              |             |
+#   |   VPP   |--- 172.16.2.1 --------vpp2 namespace-------|  veth_vpp2  |
+#   |_________|                                            | 172.16.2.2  |
+#        |                                                 |_____________|
+#     host-vpp1
+#    172.16.1.1
 #        |
 #        |
-#  vpphost (CLIENT)
-#   10.10.1.1/24
+#  vpp1 namespace
+#        |
+#   _____|_______
+#  |             |
+#  |  veth_vpp1  |
+#  | 172.16.1.2  |
+#  |_____________|
 #
 
-## create vpp and connect it to host
+# create namespaces
+sudo ip netns add vpp1
+sudo ip netns add vpp2
+
+# create and configure first veth pair
+sudo ip link add name veth_vpp1 type veth peer name vpp1
+sudo ip link set dev vpp1 up
+sudo ip link set dev veth_vpp1 up netns vpp1
+
+sudo ip netns exec vpp1 \
+  bash -c "
+    ip link set dev lo up
+    ip addr add 172.16.1.2/24 dev veth_vpp1
+    ip route add 172.16.2.0/24 via 172.16.1.1
+  "
+
+# create and configure second veth pair
+sudo ip link add name veth_vpp2 type veth peer name vpp2
+sudo ip link set dev vpp2 up
+sudo ip link set dev veth_vpp2 up netns vpp2
+
+sudo ip netns exec vpp2 \
+  bash -c "
+    ip link set dev lo up
+    ip addr add 172.16.2.2/24 dev veth_vpp2
+    ip route add 172.16.1.0/24 via 172.16.2.1
+  "
+
+# create and configure vpp instance
 sudo vpp unix { log /tmp/vpp.log cli-listen /run/vpp/cli-vpp.sock } api-segment { prefix vpp } plugins { plugin dpdk_plugin.so { disable } }
 sleep 1
 
-# veth interface between host and vpp
-sudo ip link add name vppout type veth peer name vpphost
-sudo ip link set dev vppout up
-sudo ip link set dev vpphost up
-sudo ip addr add 10.10.1.1/24 dev vpphost
+sudo vppctl -s /run/vpp/cli-vpp.sock create host-interface name vpp1
+sudo vppctl -s /run/vpp/cli-vpp.sock create host-interface name vpp2
+sudo vppctl -s /run/vpp/cli-vpp.sock set int state host-vpp1 up
+sudo vppctl -s /run/vpp/cli-vpp.sock set int state host-vpp2 up
+sudo vppctl -s /run/vpp/cli-vpp.sock set int ip address host-vpp1 172.16.1.1/24
+sudo vppctl -s /run/vpp/cli-vpp.sock set int ip address host-vpp2 172.16.2.1/24
 
-# host interface between host and vpp
-sudo vppctl -s /run/vpp/cli-vpp.sock create host-interface name vppout
-sudo vppctl -s /run/vpp/cli-vpp.sock set int state host-vppout up
-sudo vppctl -s /run/vpp/cli-vpp.sock set int ip address host-vppout 10.10.1.2/24
+# enable and configure mmb
+sudo vppctl -s /run/vpp/cli-vpp.sock mmb enable host-vpp1
+sudo vppctl -s /run/vpp/cli-vpp.sock mmb enable host-vpp2
+sudo vppctl -s /run/vpp/cli-vpp.sock trace add af-packet-input 10
 
-## create iperf server and connect it to vpp
-sudo ip link add name vppiperf type veth peer name iperfvpp
-sudo ip link set dev vppiperf up
-sudo ip link set dev iperfvpp up
-sudo ip addr add 10.10.2.2/24 dev iperfvpp
+# iperf3 server & client
+sudo ip netns exec vpp2 iperf3 -s -D
+sudo ip netns exec vpp1 iperf3 -c 172.16.2.2 -4 -k 1 --get-server-output
+#sudo ip netns exec vpp1 ping 172.16.2.1 -c 1 #+1 "in" pkt, +1 "out" pkt in mmb (host-vpp1 in/out)
+#sudo ip netns exec vpp1 ping 172.16.2.2 -c 1 #+2 "in" pkts, +2 "out" pkts in mmb (host-vpp1 in, host-vpp2 out, host-vpp2 in, host-vpp1 out)
 
-sudo vppctl -s /run/vpp/cli-vpp.sock create host-interface name vppiperf
-sudo vppctl -s /run/vpp/cli-vpp.sock set int state host-vppiperf up
-sudo vppctl -s /run/vpp/cli-vpp.sock set int ip address host-vppiperf 10.10.2.1/24
-
-#sudo ip route add 10.10.2.0/24 via 10.10.1.2
-#sudo ip route add 10.10.1.0/24 via 10.10.2.1
-
-sudo vppctl -s /run/vpp/cli-vpp.sock mmb enable host-vppout
-sudo vppctl -s /run/vpp/cli-vpp.sock mmb enable host-vppiperf
-iperf3 -s -B 10.10.2.2 -D
-iperf3 -c 10.10.2.2 -B 10.10.1.1 -u -4 -k 10
+# display results
 sudo vppctl -s /run/vpp/cli-vpp.sock sh run
+sudo vppctl -s /run/vpp/cli-vpp.sock sh tr
 
