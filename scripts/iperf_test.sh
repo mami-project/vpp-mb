ef4746cac8f2aec84c4eb107a06c74eb5b52c846
IurmanJ <justin.iurman@ulg.ac.be>
2017-12-16 23:03:10 +0100

    iperf test w/ a working topology

--- scripts/iperf_test.sh
+++ scripts/iperf_test.sh
@@ -1,79 +1,73 @@
 #!/bin/bash
 
-# TOPOLOGY:
-# =========
-#    _________                                              _____________
-#   |         |     host-vpp2                              |             |
-#   |   VPP   |---- 10.10.2.1 --------vpp2 namespace-------|  veth_vpp2  |
-#   |_________|                                            |  10.10.2.2  |
-#        |                                                 |_____________|
-#    host-vpp1
-#    10.10.1.1
-#        |
-#        |
-#  vpp1 namespace
-#        |
-#   _____|_______
-#  |             |
-#  |  veth_vpp1  |
-#  |  10.10.1.2  |
-#  |_____________|
-#
+# enable kernel forwarding
+sudo sysctl -w net.ipv4.ip_forward=1
+sysctl -p
 
-# create namespaces
-sudo ip netns add vpp1
-sudo ip netns add vpp2
-
-# create and configure first veth pair
-sudo ip link add name veth_vpp1 type veth peer name vpp1
-sudo ip link set dev vpp1 up
-sudo ip link set dev veth_vpp1 up netns vpp1
-
-sudo ip netns exec vpp1 \
-  bash -c "
-    ip link set dev lo up
-    ip addr add 10.10.1.2/24 dev veth_vpp1
-    ip route add 10.10.2.0/24 via 10.10.1.1
-  "
-
-# create and configure second veth pair
-sudo ip link add name veth_vpp2 type veth peer name vpp2
-sudo ip link set dev vpp2 up
-sudo ip link set dev veth_vpp2 up netns vpp2
-
-sudo ip netns exec vpp2 \
-  bash -c "
-    ip link set dev lo up
-    ip addr add 10.10.2.2/24 dev veth_vpp2
-    ip route add 10.10.1.0/24 via 10.10.2.1
-  "
-
-# create and configure vpp instance
+# create vpp instance
 sudo vpp unix { log /tmp/vpp.log cli-listen /run/vpp/cli-vpp.sock } api-segment { prefix vpp } plugins { plugin dpdk_plugin.so { disable } }
 sleep 1
 
-sudo vppctl -s /run/vpp/cli-vpp.sock create host-interface name vpp1
-sudo vppctl -s /run/vpp/cli-vpp.sock create host-interface name vpp2
-sudo vppctl -s /run/vpp/cli-vpp.sock set int state host-vpp1 up
-sudo vppctl -s /run/vpp/cli-vpp.sock set int state host-vpp2 up
-sudo vppctl -s /run/vpp/cli-vpp.sock set int ip address host-vpp1 10.10.1.1/24
-sudo vppctl -s /run/vpp/cli-vpp.sock set int ip address host-vpp2 10.10.2.1/24
+# ns0 namespace
+sudo ip netns add ns0
+sudo ip link add vpp0 type veth peer name vethns0
+sudo ip link set vethns0 netns ns0
+sudo ip netns exec ns0 ip link set lo up
+sudo ip netns exec ns0 ip link set vethns0 up
+sudo ip netns exec ns0 ip addr add 10.0.0.1/24 dev vethns0
+sudo ip netns exec ns0 ethtool -K vethns0 rx off tx off
+sudo ethtool --offload vpp0 rx off tx off
+sudo ip link set vpp0 up
 
-# enable and configure mmb
-sudo vppctl -s /run/vpp/cli-vpp.sock mmb enable host-vpp1
-sudo vppctl -s /run/vpp/cli-vpp.sock mmb enable host-vpp2
-sudo vppctl -s /run/vpp/cli-vpp.sock sh run
-#sudo vppctl -s /run/vpp/cli-vpp.sock trace add af-packet-input 50
+# switching
+sudo vppctl -s /run/vpp/cli-vpp.sock create host-interface name vpp0
+sudo vppctl -s /run/vpp/cli-vpp.sock set interface state host-vpp0 up
+sudo vppctl -s /run/vpp/cli-vpp.sock set interface l2 bridge host-vpp0 1
+sudo vppctl -s /run/vpp/cli-vpp.sock create loopback interface
+sudo vppctl -s /run/vpp/cli-vpp.sock set interface l2 bridge loop0 1 bvi
+sudo vppctl -s /run/vpp/cli-vpp.sock set interface state loop0 up
+sudo vppctl -s /run/vpp/cli-vpp.sock set interface ip address loop0 10.0.0.10/24
 
-# iperf3 server & client
-#sudo ip netns exec vpp2 iperf3 -s -D
-#sudo ip netns exec vpp1 iperf3 -c 10.10.2.2 -4 -k 1 --get-server-output
-#sudo ip netns exec vpp1 ping 10.10.2.1 -c 1 #+1 "in" pkt, +1 "out" pkt in mmb (host-vpp1 in/out)
-#sudo ip netns exec vpp1 ping 10.10.2.2 -c 1 #+2 "in" pkts, +2 "out" pkts in mmb (host-vpp1 in, host-vpp2 out, host-vpp2 in, host-vpp1 out)
-sudo ip netns exec vpp1 ping 10.10.2.2 -c 5
-sudo ip netns exec vpp2 ping 10.10.1.2 -c 5
+# ns2 namespace
+sudo vppctl -s /run/vpp/cli-vpp.sock tap connect tap0
+sudo ip netns add ns2
+sudo ip link set tap0 netns ns2
+sudo ip netns exec ns2 ip link set lo up
+sudo ip netns exec ns2 ip link set tap0 up
+sudo ip netns exec ns2 ip addr add 10.0.1.1/24 dev tap0
 
-# display results
-sudo vppctl -s /run/vpp/cli-vpp.sock sh run
+# routing
+sudo vppctl -s /run/vpp/cli-vpp.sock set interface state tap-0 up
+sudo vppctl -s /run/vpp/cli-vpp.sock set interface ip address tap-0 10.0.1.10/24
+sudo ip netns exec ns0 ip route add default via 10.0.0.10
+sudo ip netns exec ns2 ip route add default via 10.0.1.10
+
+# fill ARP cache
+sudo ip netns exec ns0 ping 10.0.1.1 -c 5
+sudo ip netns exec ns2 ping 10.0.0.1 -c 5
+
+# enable MMB
+sudo vppctl -s /run/vpp/cli-vpp.sock mmb enable host-vpp0
+sudo vppctl -s /run/vpp/cli-vpp.sock mmb enable tap-0
+#sudo vppctl -s /run/vpp/cli-vpp.sock trace add af-packet-input 100
+#sudo vppctl -s /run/vpp/cli-vpp.sock sh run
 #sudo vppctl -s /run/vpp/cli-vpp.sock sh tr
+
+# iperf server
+sudo ip netns exec ns2 iperf3 -s -D -B 10.0.1.1
+sleep 1
+
+# iperf client (test & results)
+sudo ip netns exec ns0 iperf3 -c 10.0.1.1 -4 -k 1 -V --get-server-output
+sleep 1
+#sudo vppctl -s /run/vpp/cli-vpp.sock sh run
+#sudo vppctl -s /run/vpp/cli-vpp.sock sh tr
+#sleep 1
+
+# cleanup
+sudo kill -9 $(ps -aef | grep -v grep | grep iperf3 | awk '{print $2}')
+sudo kill -9 $(ps -aef | grep -v grep | grep vpp | awk '{print $2}')
+sudo ip link del vpp0
+sudo ip netns del ns0
+sudo ip netns del ns2
 
