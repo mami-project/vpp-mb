6b56539810817ba1d8fb8b5a02d4156d15f3686e
IurmanJ <justin.iurman@ulg.ac.be>
2017-12-20 15:47:05 +0100

    iperf test w/o tcp options

--- scripts/iperf_test.sh
+++ scripts/iperf_test.sh
@@ -1,5 +1,107 @@
 #!/bin/bash
 
+if [ $# -lt 1 ] || [ $# -gt 2 ]; then
+  echo "Syntax: $0 <rules number> [--with-tcp-options]"
+  exit 1
+fi
+
+if [ "$1" != 10 ] && [ "$1" != 100 ] && [ "$1" != 1000 ] && [ "$1" != 10000 ]; then
+  echo "Rules number must be either 10, 100, 1000 or 10000"
+  exit 1
+fi
+
+if [ "$2" != "" ] && [ "$2" != "--with-tcp-options" ]; then
+  echo "Syntax: $0 <rules number> [--with-tcp-options]"
+  exit 1
+fi
+
+function get_random_match
+{
+  case $(( $1 % 23 )) in
+    0)
+      echo "ip-ver $(( RANDOM % 16 ))";;
+    1)
+      echo "ip-ihl $(( RANDOM % 16 ))";;
+    2)
+      echo "ip-dscp $(( RANDOM % 64 ))";;
+    3)
+      echo "ip-ecn $(( RANDOM % 4 ))";;
+    4)
+      echo "ip-len $(( RANDOM % 65536 ))";;
+    5)
+      echo "ip-id $(( RANDOM % 65536 ))";;
+    6)
+      echo "ip-flags $(( RANDOM % 8 ))";;
+    7)
+      echo "ip-frag-offset $(( RANDOM % 8192 ))";;
+    8)
+      echo "ip-ttl $(( RANDOM % 256 ))";;
+    9)
+      echo "ip-proto $(( RANDOM % 256 ))";;
+    10)
+      echo "ip-checksum $(( RANDOM % 65536 ))";;
+    11)
+      a=$(( RANDOM % 256 ))
+      b=$(( RANDOM % 256 ))
+      c=$(( RANDOM % 256 ))
+      d=$(( RANDOM % 256 ))
+      echo "ip-saddr $a.$b.$c.$d";;
+    12)
+      a=$(( RANDOM % 256 ))
+      b=$(( RANDOM % 256 ))
+      c=$(( RANDOM % 256 ))
+      d=$(( RANDOM % 256 ))
+      echo "ip-daddr $a.$b.$c.$d";;
+    13)
+      echo "tcp-sport $(( RANDOM % 65536 ))";;
+    14)
+      echo "tcp-dport $(( RANDOM % 65536 ))";;
+    15)
+      echo "tcp-seq-num $(( RANDOM % 4294967296 ))";;
+    16)
+      echo "tcp-ack-num $(( RANDOM % 4294967296 ))";;
+    17)
+      echo "tcp-offset $(( RANDOM % 16 ))";;
+    18)
+      echo "tcp-reserved $(( RANDOM % 16 ))";;
+    19)
+      echo "tcp-flags $(( RANDOM % 256 ))";;
+    20)
+      echo "tcp-win $(( RANDOM % 65536 ))";;
+    21)
+      echo "tcp-checksum $(( RANDOM % 65536 ))";;
+    22)
+      echo "tcp-urg-ptr $(( RANDOM % 65536 ))";;
+  esac
+}
+
+function get_random_target
+{
+  echo "mod ip-ttl 50"
+}
+
+function rule_gen
+{
+  RULES=$1
+  OPTIONS=$2
+
+  #echo "Number of rules to generate: $RULES"
+  #if $OPTIONS; then
+  #  echo "With TCP options"
+  #fi
+
+  i=0
+  until [ $i -eq $RULES ]; do
+    match=$(get_random_match $i)
+    target=$(get_random_target $i)
+
+    sudo vppctl -s /run/vpp/cli-vpp.sock mmb add $match $target
+    let i+=1
+  done
+
+  #TODO how many matches in matching part ? how many targets in target part ?
+}
+
 # enable kernel forwarding
 sudo sysctl -w net.ipv4.ip_forward=1
 sysctl -p
@@ -53,12 +155,17 @@ sudo vppctl -s /run/vpp/cli-vpp.sock mmb enable tap-0
 #sudo vppctl -s /run/vpp/cli-vpp.sock sh run
 #sudo vppctl -s /run/vpp/cli-vpp.sock sh tr
 
+# fill MMB rule table
+[[ $2 = "--with-tcp-options" ]] && tcpopts=true || tcpopts=false
+rule_gen $1 $tcpopts
+
 # iperf server
 sudo ip netns exec ns2 iperf3 -s -D -B 10.0.1.1
 sleep 1
 
 # iperf client (test & results)
-sudo ip netns exec ns0 iperf3 -c 10.0.1.1 -4 -k 1 -V --get-server-output
+#TODO speed gen ? pass it as an argument ?
+sudo ip netns exec ns0 iperf3 -c 10.0.1.1 -4 -k 10000 -V --get-server-output
 sleep 1
 #sudo vppctl -s /run/vpp/cli-vpp.sock sh run
 #sudo vppctl -s /run/vpp/cli-vpp.sock sh tr
