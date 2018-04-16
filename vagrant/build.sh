fa28b5f888b16c39a0f7212b85f7510873a559b0
korian <korian.edeline@ulg.ac.Be>
2018-04-16 09:21:03 +0200

    remove unused plugin from vm

--- vagrant/build.sh
+++ vagrant/build.sh
@@ -9,3 +9,14 @@ apt-get install -y git vpp vpp-lib vpp-dev autoconf libtool traceroute python-sc
 service vpp stop
 (cd /home/vagrant/vpp-mb/mmb-plugin; autoreconf -fis; ./configure; make; make install)
 
+rm /usr/lib/vpp_plugins/acl_plugin.so
+rm /usr/lib/vpp_plugins/dpdk_plugin.so
+rm /usr/lib/vpp_plugins/flowprobe_plugin.so
+rm /usr/lib/vpp_plugins/gtpu_plugin.so
+rm /usr/lib/vpp_plugins/ila_plugin.so
+rm /usr/lib/vpp_plugins/ioam_plugin.so
+rm /usr/lib/vpp_plugins/ixge_plugin.so
+rm /usr/lib/vpp_plugins/lb_plugin.so
+rm /usr/lib/vpp_plugins/libsixrd_plugin.so
+rm /usr/lib/vpp_plugins/pppoe_plugin.so
+
