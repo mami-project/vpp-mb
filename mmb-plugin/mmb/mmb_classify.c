284a88d4821e6280513daa4bfbafe94ee172753c
korian <korian.edeline@ulg.ac.Be>
2018-05-08 15:40:52 +0200

    stop matching when first drop rule is matched

--- mmb-plugin/mmb/mmb_classify.c
+++ mmb-plugin/mmb/mmb_classify.c
@@ -244,7 +244,7 @@ mmb_classify_inline (vlib_main_t * vm,
                   hits++;
               } 
               
-              while (1) {
+              while (next0 != MMB_CLASSIFY_NEXT_INDEX_DROP) {
                  if (t0->next_table_index != ~0)
                    t0 = pool_elt_at_index (vcm->tables,
                                              t0->next_table_index);
